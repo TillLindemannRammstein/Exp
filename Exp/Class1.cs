59905337ce1714499e58033d4d5f49803258df34
KungFu-Padla <t50_a.k.antonov@mpt.ru>
2023-02-06 10:57:44 +0300

    Добавьте файлы проекта.

---
+++ Exp/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp
+{
+    internal class Class1
+    {
+        private string Name;
+        private string Path;
+
+        public Class1(string name, string path)
+        {
+            Name = name;
+        }
+
+    }
+
+
+
+
+}
+
